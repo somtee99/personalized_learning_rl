3a2dd5c40bbd934d18c3b1a473bea4d8bd4b756d
somtee99 <okaforsomtoo007@gmail.com>
2025-07-23 15:20:01 +0100

    Refactor teacher agent to improve structure and add directory creation for training and model storage. Updated environment initialization and training process.

--- agents/teacher_agent.py
+++ agents/teacher_agent.py
@@ -1,9 +1,15 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from environment.question_selection_env import QuestionSelectionEnv
 import pandas as pd
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
-from environment.question_selection_env import QuestionSelectionEnv
 
 df = pd.read_csv("data/cleaned_df.csv")
+os.makedirs("../trainings", exist_ok=True)
+os.makedirs("../models", exist_ok=True)
 
 def clean_skill_list(skill_str):
     if pd.isnull(skill_str):
@@ -13,11 +19,10 @@ def clean_skill_list(skill_str):
 df['skill_name_split'] = df['skill_name'].apply(clean_skill_list)
 all_skills = sorted({skill for skills in df['skill_name_split'] for skill in skills})
 
-print(all_skills)
-
-env = QuestionSelectionEnv(all_skills=all_skills)
+env = QuestionSelectionEnv()
 
 vec_env = make_vec_env(lambda: env, n_envs=1)
-model = PPO("MlpPolicy", vec_env, verbose=1)
-model.learn(total_timesteps=10000)
-model.save("ppo_teacher_agent")
+model = PPO("MlpPolicy", vec_env, verbose=1, tensorboard_log="../trainings")
+print("Starting training...")
+model.learn(total_timesteps=100000)
+model.save("../models/ppo_teacher_agent")
